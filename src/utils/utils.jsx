929d30297636f9b59d3b34a2007ca4e365c4a584
Aman Kumar Mahto <61381501+mahtoaman@users.noreply.github.com>
2023-03-20 23:07:10 +0530

    Add files via upload

---
+++ src/utils/utils.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+const token = localStorage.getItem("token");
+
+const baseUrl = "http://localhost:4000";
+
+const getAllToDo = (setToDo) => {
+  axios.get(`${baseUrl}/gettodo`,{
+     headers: {
+          authorization: `${token}`,
+        },
+  }).then(({ data }) => {
+    console.log("data ---> ", data.data);
+    setToDo(data.data);
+  });
+};
+
+const addToDo = (text, setText, setToDo) => {
+  axios
+    .post(`${baseUrl}/save`, { text })
+    .then((data) => {
+      console.log(data);
+      toast.success("Task added successfully");
+      setText("");
+      getAllToDo(setToDo);
+    })
+    .catch((err) => console.log(err));
+};
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+  axios
+    .put(`${baseUrl}/update`, { _id: toDoId, text })
+    .then((data) => {
+      toast.success("Task updated successfully");
+      setText("");
+      setIsUpdating(false);
+      getAllToDo(setToDo);
+    })
+    .catch((err) => console.log(err));
+};
+
+const deleteToDo = (_id, setToDo) => {
+  axios
+    .post(`${baseUrl}/delete`, { _id })
+    .then((data) => {
+      console.log(data);
+      toast.success("Task deleted successfully");
+
+      getAllToDo(setToDo);
+    })
+    .catch((err) => console.log(err));
+};
+
+export { getAllToDo, updateToDo, addToDo, deleteToDo };
